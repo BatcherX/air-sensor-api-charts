76a5017fa0c103a2660b86b2d7d0b809fd7a59c2
Artur Gołdyn <artur.goldyn@richapps.pl>
2017-10-05 22:32:07 +0200

    Calculate averages from minutes.

--- server.js
+++ server.js
@@ -6,12 +6,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/api/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'server/assets/index.html'));
-});
+const dataFolder = './server/data';
 
-app.get('/api/listDays/', (req, res) => {
-  const dataFolder = './server/data';
+const getAllDays = () => {
   const fs = require('fs');
   const regex = /smog_(\d+-\d+-\d+).txt/;
   var days = [];
@@ -21,10 +18,90 @@ app.get('/api/listDays/', (req, res) => {
       var day = matchDate[1];
       days.push(day);
     }
-  })  
+  })
+  return days;
+}
+
+function pad(n) {
+  return (n < 10) ? ("0" + n) : n;
+}
+
+const readDayFile = (day) => {
+  const filename = dataFolder + '/smog_' + day + '.txt';
+  fs = require('fs');
+  return fs.readFileSync(filename);
+}
+
+app.get('/api/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'server/assets/index.html'));
+});
+
+app.get('/api/listDays/', (req, res) => {
+  const days = getAllDays();
   res.json(days);
   console.log("listDays: ", req.ip);
 });
+
+app.get("/api/:day/minutes/", (req, res) => {
+  const days = getAllDays();
+  const day = req.params.day;
+  const index = days.indexOf(day);
+
+  if(index === -1) {
+    res.status(404).send('No such day in list!');
+  } else {
+    const indexOfDay = days[index];
+    const file = readDayFile(indexOfDay);
+    const allRows = file.toString().split(/[\r\n]+/);
+    var minutes = [];
+    for(i = 0; i < allRows.length; ++i){
+      if(allRows[i].length === 0) {
+        break;
+      }
+      const rowRegex = /^([0-9]{2} [A-Za-z]{3} [0-9]+) ([0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]+)\: (.*)$/;
+      var rowData = allRows[i].match(rowRegex);
+      var date = rowData[1];
+      var time = rowData[2];
+      var measurments = rowData[3];
+
+      const dustRegex = /^PM2\.5 (.*) mcg\/m3, PM10: (.*) mcg\/m3$/;
+      var dust = measurments.match(dustRegex);
+      var PM2_5 = dust[1];
+      var PM10 = dust[2];
+
+      var timestamp = Date.parse(date + ' ' + time);
+      var datetime = new Date(timestamp);
+      
+      var minute = datetime.getMinutes();
+      var hour = datetime.getHours();
+      var second = datetime.getSeconds();
+
+      var minuteIndex = pad(hour) +":" + pad(minute);
+
+      if(minutes[minuteIndex]) {
+        minutes[minuteIndex].seconds.push({second: second, pm2_5: parseFloat(PM2_5), pm10: parseFloat(PM10)});
+        minutes[minuteIndex].sumPM2_5+=parseFloat(PM2_5);
+        minutes[minuteIndex].sumPM10+=parseFloat(PM10);
+      } else {
+        minutes[minuteIndex] = {
+           seconds:[{second: second, pm2_5: parseFloat(PM2_5), pm10: parseFloat(PM10)}],
+           sumPM2_5:parseFloat(PM2_5),
+           sumPM10:parseFloat(PM10),
+        }
+      }  
+    }
+
+    var averages = [];
+    for(var key in minutes){
+      averages.push({
+        minute: key,
+        pm10: minutes[key].sumPM10 / minutes[key].seconds.length,
+        pm2_5: minutes[key].sumPM2_5 / minutes[key].seconds.length,
+      });
+    }
+    res.send(averages);
+  }
+})
 
 /**
  * Get port from environment and store in Express.
